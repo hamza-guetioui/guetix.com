19ff11c7c0834bb6cda89686276f5eec5a941616
lamaachi <istatasila7@gmail.com>
2025-03-01 13:58:37 +0000

    setup the website

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,7 @@ import styles from "./home.module.css"
 
 const Index = () => {
   return (
-    <div className={`w-full h-[96vh] bg-blue-950 rounded-2xl pt-16  ${styles.curved_div}`}>GUETIX</div>
+    <div className={`w-full h-[96vh] bg-blue-950 rounded-2xl pt-16  ${styles.curved_div}`}>GUETIX.</div>
   )
 }
 
