5c1a25dbdf9ff912b9416091c2d7d080f833a5a2
Guetix <hamzagatoui09@gmail.com>
2025-04-13 19:31:27 +0100

    refactor: restructure app routes into (admin) and (main) segments

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import Header from "./Header";
 
 const Index = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <Header />
       {children}
     </>
   );
